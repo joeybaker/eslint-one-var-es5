05af0129ea2ecd9203bd62b3825b9f1d256fc0bb
Joey Baker <joey@byjoeybaker.com>
2015-04-14 12:42:51 -0700

    Fix rule paths #oops

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 module.exports = {
   rules: {
     "one-var-es6": require("./rules/one-var-es6.js"),
-    "one-let": require("./lib/rules/one-let.js"),
-    "one-const": require("./lib/rules/one-const.js")
+    "one-let": require("./rules/one-let.js"),
+    "one-const": require("./rules/one-const.js")
   },
   rulesConfig: {
     "one-var-es6": [1, "always"],
